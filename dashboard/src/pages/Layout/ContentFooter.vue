3032629689936ed248f602867bfa0dcfcbafae87
Jonathan <jonathanadcz@gmail.com>
2021-10-13 23:39:25 -0300

    Versao Projeto Restaurante

---
+++ dashboard/src/pages/Layout/ContentFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.jonathandacruz.com.br">Saiba mais!</a>
+          </li>
+ 
+        </ul>
+      </nav>
+      <div class="copyright text-center">
+        &copy; {{ new Date().getFullYear() }}
+         Pratica Integrada  II
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
---
+++ dashboard/src/pages/Layout/DashboardLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
+    <notifications></notifications>
+
+    <side-bar
+      :sidebar-item-color="sidebarBackground"
+      :sidebar-background-image="sidebarBackgroundImage"
+    >
+      <mobile-menu slot="content"></mobile-menu>
+      <sidebar-link to="/dashboard">
+        <md-icon>dashboard</md-icon>
+        <p>Painel Principal</p>
+      </sidebar-link>
+      <sidebar-link to="/user">
+        <md-icon>person</md-icon>
+        <p>Usuários</p>
+      </sidebar-link>
+      <sidebar-link to="/table">
+        <md-icon>content_paste</md-icon>
+        <p>Últimos Pedidos</p>
+      </sidebar-link>
+      <sidebar-link to="/typography">
+        <md-icon>library_books</md-icon>
+        <p>Cardápio</p>
+      </sidebar-link>
+      <sidebar-link to="/icons">
+        <md-icon>bubble_chart</md-icon>
+        <p>Cadastros</p>
+      </sidebar-link>
+      <sidebar-link to="/notifications">
+        <md-icon>notifications</md-icon>
+        <p>Cozinha</p>
+      </sidebar-link>
+       
+    </side-bar>
+
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <fixed-plugin
+        :color.sync="sidebarBackground"
+        :image.sync="sidebarBackgroundImage"
+      >
+      </fixed-plugin>
+
+      <dashboard-content> </dashboard-content>
+
+      <content-footer v-if="!$route.meta.hideFooter"></content-footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import ContentFooter from "./ContentFooter.vue";
+import DashboardContent from "./Content.vue";
+import MobileMenu from "@/pages/Layout/MobileMenu.vue";
+import FixedPlugin from "./Extra/FixedPlugin.vue";
+
+export default {
+  components: {
+    TopNavbar,
+    DashboardContent,
+    ContentFooter,
+    MobileMenu,
+    
+  },
+  data() {
+    return {
+      sidebarBackground: "red",
+      sidebarBackgroundImage: require("@/assets/img/sidebar-2.jpg"),
+    };
+  },
+};
+</script>
